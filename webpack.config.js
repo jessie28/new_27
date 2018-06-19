695ccede6548f8fb0ba733274730c81316ab7f5c
zhangxin <xinzhang@mjitech.com>
2018-06-19 19:29:04 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const webpack = require('webpack')
+
+let baseConfig = {
+    context : path.join(__dirname,'./client'),
+    entry : {
+
+    },
+    module : {
+
+    }
+}
+module.exports = Object.assign({},baseConfig);
